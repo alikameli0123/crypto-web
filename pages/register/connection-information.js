2451ce1b90a13f5d043da15bbaf4fea209e04e51
alikameli <alikameli0123@gmail.com>
2023-04-11 10:30:52 +0430

    connection information

--- pages/register/connection-information.js
+++ pages/register/connection-information.js
@@ -1,16 +1,110 @@
 import Head from 'next/head'
+import Image from 'next/image';
 import Link from 'next/link';
+import React, { useState, useEffect, useRef } from 'react';
+import Register from '@/src/Components/Register/Register';
+import style from "../../styles/connection_information.module.css";
+import Countdown, { zeroPad } from "react-countdown";
 
 const ConnectionInformationPage = () => {
 
+  const [number, setNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [fill, setFill] = useState(false);
+  const [sendCode, setSendCode] = useState(false);
+
+  const firstTxt = useRef();
+  const secTxt = useRef();
+  const threeTxt = useRef();
+  const fourTxt = useRef();
+
+  useEffect(() => {
+    if (number.length > 0 && email.length > 0) {
+      setFill(true);
+    } else {
+    }
+  }, [number, email])
+
+  const getDetail = () => { }
+  
+  const sendCodeHandler = () => {
+    if (number.length === 11) {
+      setSendCode(true);
+      console.log('send code');
+    }
+  }
+
+  const verifyPhoneNumber = (e) => {
+    e.preventDefault();
+    const firstValue = firstTxt.current.value;
+    const secValue = secTxt.current.value;
+    const threeValue = threeTxt.current.value;
+    const fourValue = fourTxt.current.value;
+
+    console.log(firstValue+secValue+threeValue+fourValue)
+  }
+  // for focus on verify input code
+  const handleFocus = (e) => {
+    if (e.target.nextSibling) e.target.nextSibling.focus();
+  }
 
   return (
     <>
       <Head>
-        <title>Connection Information</title>
+        <title>personal Information</title>
       </Head>
-      <Link href='/register/place-information'> place information</Link>
+      <Register stepNumber='2' next_link='place-information' getDetail={getDetail} checkFill={fill} >
 
+        <form method='POST'>
+          {/* Phone number */}
+          <div className={style.input_container}>
+            <div className={style.label_parrent}>
+              <label className={style.label} for='#number'> شماره همراه</label>
+            </div>
+            <div className={style.input_parrent}>
+              <span className={style.sendCode} onClick={sendCodeHandler}>ارسال کد</span>
+              <input className={style.input} maxLength='11' onChange={(e) => { setNumber(e.target.value) }} id='number' placeholder='09112564798' />
+              <span className={style.icon}>
+                <Image src='/assets/register/mobile.png' width={20} height={20} alt='number' />
+              </span>
+            </div>
+          </div>
+          {/* Send Code */}
+          {
+            sendCode &&
+            <div className={style.sendCodeParrent}>
+              <div className={style.sendCodeDetail}>
+                <p>کد تائید به شماره {number} ارسال شده است. این کد تا
+                  <Countdown date={Date.now() + 120000} daysInHours={true} />
+                  دقیقه دیگر معتبر است</p>
+                <Image src='/assets/register/clipboardtick.png' width={20} height={20} alt='clipboardtick' />
+              </div>
+              <div className={style.confirmVerify}>
+                <p>کد تائید</p>
+                <div className={style.verifyCode}>
+                  <input onChange={handleFocus} ref={firstTxt} type="text" maxLength="1" autoFocus={true} />
+                  <input onChange={handleFocus} ref={secTxt} type="text" maxLength="1" />
+                  <input onChange={handleFocus} ref={threeTxt} type="text" maxLength="1" />
+                  <input onChange={handleFocus} ref={fourTxt} type="text" maxLength="1" />
+                </div>
+                <button className={style.confirmNumberBtn} onClick={verifyPhoneNumber}>تائید شماره همراه</button>
+              </div>
+            </div>
+          }
+          {/* Email */}
+          <div className={style.input_container}>
+            <div className={style.label_parrent}>
+              <label className={style.label} for='#email'> ایمیل </label>
+            </div>
+            <div className={style.input_parrent}>
+              <input className={style.input} type='email' onChange={(e) => { setEmail(e.target.value) }} id='email' placeholder='example@mail.com' />
+              <span className={style.icon}>
+                <Image src='/assets/register/message.png' width={20} height={20} alt='email' />
+              </span>
+            </div>
+          </div>
+        </form>
+      </Register>
     </>
   )
 }
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -2,11 +2,7 @@ import Link from 'next/link';
 import React, { useEffect } from 'react';
 import style from "./Register.module.css";
 
-const Register = ({ children, stepNumber, next_link, getDetail, checkFill }) => {
-
-    useEffect(() => {
-        console.log(checkFill)
-    })
+const Register = ({ children, stepNumber,prev_link, next_link, getDetail, checkFill }) => {
     return (
         <div className={`text-center ${style.registerFormParrent}`}>
             <h5 className={style.stepNumber}>  مرحله {stepNumber} از 3</h5>
@@ -17,9 +13,16 @@ const Register = ({ children, stepNumber, next_link, getDetail, checkFill }) => 
             <div className={style.endLine} ></div>
                 <Link href={`/register/${next_link}`} onClick={getDetail} >
             <button className={checkFill ? style.activeBtn : style.deactiveBtn} disabled={!checkFill}>
+                {/* Todo put the arrow icon from font-awesome */}
                     مرحله بعد
             </button>
                 </Link>
+                {
+                    stepNumber != 1 &&
+                    <Link href={`/register/${prev_link}`} className={style.prevLink}>
+                     مرحله قبل
+                    </Link>
+                }
         </div>
     )
 };
